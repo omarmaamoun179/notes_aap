3205edc6db211dce1fc368f2154b46b3d5b658d2
Omar Maamon <omarmaamoun6@gmail.com>
2023-02-19 03:29:26 +0200

    create addnote states

--- lib/add_note_cubit/cubit/addnote_state.dart
+++ lib/add_note_cubit/cubit/addnote_state.dart
@@ -1,6 +1,14 @@
 part of 'addnote_cubit.dart';
 
-
 abstract class AddnoteState {}
 
 class AddnoteInitial extends AddnoteState {}
+
+class AddnoteLoading extends AddnoteState {}
+
+class AddnoteSuccess extends AddnoteState {}
+
+class AddnoteFailed extends AddnoteState {
+  String errMessage;
+  AddnoteFailed({required this.errMessage});
+}
