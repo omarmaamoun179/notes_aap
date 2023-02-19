b572cb9f0e1217a0ee019f10ca258beb77196d56
Omar Maamon <omarmaamoun6@gmail.com>
2023-02-19 03:27:40 +0200

    creat add note cubit files

---
+++ lib/add_note_cubit/cubit/addnote_cubit.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'addnote_state.dart';
+
+class AddnoteCubit extends Cubit<AddnoteState> {
+  AddnoteCubit() : super(AddnoteInitial());
+}
---
+++ lib/add_note_cubit/cubit/addnote_state.dart
@@ -0,0 +1,6 @@
+part of 'addnote_cubit.dart';
+
+
+abstract class AddnoteState {}
+
+class AddnoteInitial extends AddnoteState {}
