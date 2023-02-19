613df6aa75ccb816b2e1e9400149f0b3e4cc8c7e
Omar Maamon <omarmaamoun6@gmail.com>
2023-02-19 03:58:54 +0200

    add muliti bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:notes_app/add_note_cubit/cubit/addnote_cubit.dart';
 import 'package:notes_app/constant.dart';
 import 'package:notes_app/model/note_model.dart';
 import 'package:notes_app/views/note_screen.dart';
@@ -16,13 +18,18 @@ class NotesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: 'Poppins',
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: ((context) => AddnoteCubit()),),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: 'Poppins',
+        ),
+        home: const NoteScreen(),
       ),
-      home: const NoteScreen(),
     );
   }
 }
