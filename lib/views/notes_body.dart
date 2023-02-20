3e7868a7cf82059e97784f36cbf23e982b0aafbc
Omar Maamon <omarmaamoun6@gmail.com>
2023-02-20 10:46:58 +0200

    delete note

--- lib/views/notes_body.dart
+++ lib/views/notes_body.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:notes_app/constant.dart';
+import 'package:notes_app/model/note_model.dart';
 import 'package:notes_app/views/edit_note_view.dart';
 import 'package:notes_app/widgets/custom_appbar.dart';
+import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/note_item.dart';
 import 'package:notes_app/widgets/note_list.dart';
 
@@ -15,36 +19,44 @@ class NotesBody extends StatefulWidget {
 }
 
 class _NotesBodyState extends State<NotesBody> {
-   @override
-  void initState() { 
+  @override
+  void initState() {
     BlocProvider.of<NotesCubit>(context).fetchAllNotes();
     super.initState();
-    
   }
+
+  NoteModel? noteModel;
   @override
   Widget build(BuildContext context) {
-    return GestureDetector
-    
-    (
-      onTap: () => Navigator.push(context,  MaterialPageRoute(builder: ((context) => EditNoteView()),),),
+    return GestureDetector(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: ((context) => EditNoteView()),
+        ),
+      ),
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
-          children: const [
+          children: [
             SizedBox(
               height: 55,
             ),
             CustomAppBar(
               title: 'Notes',
-              icon:Icon( Icons.search),
+              icon: Icon(Icons.search),
+            
+              
             ),
          
-           
-            Expanded(child: NoteListviewItem()),
+            Expanded(
+              child: NoteListviewItem(),
+            ),
+            
+            
           ],
         ),
       ),
     );
   }
 }
-
--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -8,25 +8,23 @@ class CustomAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      // ignore: prefer_const_literals_to_create_immutables
-      children: [
-        Text(
-          title,
-          style: TextStyle(fontSize: 24),
-        ),
-        Spacer(),
-        Container(
-            height: 45,
-            width: 45,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(.05),
-              borderRadius: BorderRadius.circular(16),
-            ),
-            child: IconButton(
-              onPressed: () {},
-              icon: icon
-            )),
-      ],
-    );
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          Text(
+            title,
+            style: TextStyle(fontSize: 24),
+          ),
+          Spacer(),
+          Container(
+              height: 45,
+              width: 45,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(.05),
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: IconButton(onPressed: () {}, icon: icon)),
+              
+        
+        ]);
   }
 }
